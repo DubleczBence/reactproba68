145d0f792b25479a8e5c7d49c61605d26ebf81de
DubleczBence <dubleczbence@gmail.com>
2025-01-15 12:29:24 +0100

    20250115

--- src/Home.js
+++ src/Home.js
@@ -1,69 +1,34 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 
 
-const vegzettsegek = [
-  'Egyetem, főiskola stb. oklevéllel',
-  'Középfokú végzettség éretségi nélkül, szakmai végzetséggel',
-  'Középfokú végzettség éretségivel (szakmai végzetség nélkül)',
-  'Középfokú végzettség éretségivel (szakmai végzetségel)',
-  'Általános iskola 8. osztálya',
-  '8 általános iskolánál kevesebb',
-];
 
 
-
-function getStyles(vegzettseg, VegzettsegName, theme) {
-  return {
-    fontWeight: VegzettsegName.includes(vegzettseg)
-      ? theme.typography.fontWeightMedium
-      : theme.typography.fontWeightRegular,
-  };
-}
 
 
 const Home = ({ onSignOut }) => {
 
 
-  const theme = useTheme();
-  const [VegzettsegName, setVegzettsegName] = React.useState([]);
+  const [vegzettseg, setVegzettseg] = React.useState('');
   const [korcsoport, setKorcsoport] = React.useState('');
   const [regio, setRegio] = React.useState('');
+  const [nem, setNem] = React.useState('');
+  const [anyagi, setAnyagi] = React.useState('');
 
 
 
-  const handleVegzettseg= (event) => {
-    const {
-      target: { value },
-    } = event;
-    setVegzettsegName(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+
+
+  const handleVegzettseg = (event) => {
+    setVegzettseg(event.target.value);
   };
 
-
+  
 
   const handleKorcsoport = (event) => {
     setKorcsoport(event.target.value);
@@ -73,6 +38,18 @@ const Home = ({ onSignOut }) => {
   const handleRegio = (event) => {
     setRegio(event.target.value);
   };
+
+
+  const handleNem = (event) => {
+    setNem(event.target.value);
+  };
+
+
+
+  const handleAnyagi = (event) => {
+    setAnyagi(event.target.value);
+  };
+
 
 
 
@@ -87,38 +64,33 @@ const Home = ({ onSignOut }) => {
       <button onClick={onSignOut}>Kijelentkezés</button>
 
 
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-chip-label">Végzettség</InputLabel>
+      
+      <FormControl sx={{ m: 1, minWidth: 240 }}>
+        <InputLabel id="demo-simple-select-autowidth-label">Legmagasabb végzettség</InputLabel>
         <Select
-          labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip"
-          multiple
-          value={VegzettsegName}
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={vegzettseg}
           onChange={handleVegzettseg}
-          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
+          autoWidth
+          label="Legmagasabb végzettség"
         >
-          {vegzettsegek.map((vegzettseg) => (
-            <MenuItem
-              key={vegzettseg}
-              value={vegzettseg}
-              style={getStyles(vegzettseg, VegzettsegName, theme)}
-            >
-              {vegzettseg}
-            </MenuItem>
-          ))}
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={1}>Egyetem, főiskola stb. oklevéllel</MenuItem>
+          <MenuItem value={2}>Középfokú végzettség éretségi nélkül, szakmai végzetséggel</MenuItem>
+          <MenuItem value={3}>Középfokú végzettség éretségivel (szakmai végzetség nélkül)</MenuItem>
+          <MenuItem value={4}>Középfokú végzettség éretségivel (szakmai végzetségel)</MenuItem>
+          <MenuItem value={5}>Általános iskola 8. osztálya</MenuItem>
+          <MenuItem value={6}>8 általános iskolánál kevesebb</MenuItem>
         </Select>
       </FormControl>
 
 
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
+
+
+      <FormControl sx={{ m: 1, minWidth: 130 }}>
         <InputLabel id="demo-simple-select-autowidth-label">Korcsoport</InputLabel>
         <Select
           labelId="demo-simple-select-autowidth-label"
@@ -131,18 +103,18 @@ const Home = ({ onSignOut }) => {
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={20}>16 évesnél fiatalabb</MenuItem>
-          <MenuItem value={21}>16-15</MenuItem>
-          <MenuItem value={22}>26-35</MenuItem>
-          <MenuItem value={23}>36-45</MenuItem>
-          <MenuItem value={24}>46-55</MenuItem>
-          <MenuItem value={25}>56-65</MenuItem>
-          <MenuItem value={26}>66 éves vagy idősebb</MenuItem>
+          <MenuItem value={7}>16 évesnél fiatalabb</MenuItem>
+          <MenuItem value={8}>16-15</MenuItem>
+          <MenuItem value={9}>26-35</MenuItem>
+          <MenuItem value={10}>36-45</MenuItem>
+          <MenuItem value={11}>46-55</MenuItem>
+          <MenuItem value={12}>56-65</MenuItem>
+          <MenuItem value={13}>66 éves vagy idősebb</MenuItem>
         </Select>
       </FormControl>
 
 
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <FormControl sx={{ m: 1, minWidth: 100 }}>
         <InputLabel id="demo-simple-select-autowidth-label">Régió</InputLabel>
         <Select
           labelId="demo-simple-select-autowidth-label"
@@ -150,17 +122,60 @@ const Home = ({ onSignOut }) => {
           value={regio}
           onChange={handleRegio}
           autoWidth
-          label="Korcsoport"
+          label="Régió"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={27}>Nyugat-Dunántúl</MenuItem>
-          <MenuItem value={28}>Közép-Dunántúl</MenuItem>
-          <MenuItem value={29}>Közép-Magyarország</MenuItem>
-          <MenuItem value={30}>Észak-Magyarország</MenuItem>
-          <MenuItem value={31}>Észak-Alföldv</MenuItem>
-          <MenuItem value={32}>Dél-Alföld</MenuItem>
+          <MenuItem value={14}>Nyugat-Dunántúl</MenuItem>
+          <MenuItem value={15}>Közép-Dunántúl</MenuItem>
+          <MenuItem value={16}>Közép-Magyarország</MenuItem>
+          <MenuItem value={17}>Észak-Magyarország</MenuItem>
+          <MenuItem value={18}>Észak-Alföldv</MenuItem>
+          <MenuItem value={19}>Dél-Alföld</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl sx={{ m: 1, minWidth: 100 }}>
+        <InputLabel id="demo-simple-select-autowidth-label">Neme</InputLabel>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={nem}
+          onChange={handleNem}
+          autoWidth
+          label="Neme"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={20}>Férfi</MenuItem>
+          <MenuItem value={21}>Nő</MenuItem>
+          <MenuItem value={22}>Egyéb</MenuItem>
+        </Select>
+      </FormControl>
+
+
+
+      <FormControl sx={{ m: 1, minWidth: 220 }}>
+        <InputLabel id="demo-simple-select-autowidth-label">Havi nettó átlagkereset</InputLabel>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={anyagi}
+          onChange={handleAnyagi}
+          autoWidth
+          label="Havi nettó átlagkereset"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={23}>{'<'} 100 000 Ft</MenuItem>
+          <MenuItem value={24}>100 000 Ft - 250 000 Ft</MenuItem>
+          <MenuItem value={25}>250 000 Ft - 500 000 Ft</MenuItem>
+          <MenuItem value={26}>500 000 Ft - 1 000 000 Ft</MenuItem>
+          <MenuItem value={27}>1 000 000 Ft - 1 500 000 Ft</MenuItem>
+          <MenuItem value={28}>1 500 000 Ft {'<'}</MenuItem>
         </Select>
       </FormControl>
     </div>
