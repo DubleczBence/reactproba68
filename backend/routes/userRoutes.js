7d692f9136b5ec1fb8f6a866fbe3a0d049e54dea
DubleczBence <dubleczbence@gmail.com>
2025-04-09 01:47:24 +0200

    20250409_5

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
+const { authenticateToken } = require('../middleware/authMiddleware');
 const { authenticateUser } = require('../middleware/auth');
 const {
   validateUserRegistration,
